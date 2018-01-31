1c0fc48c9a0a43eaa53087fecfc2c7dfe541218b
Unknown <lvegna12@gmail.com>
2018-01-31 15:06:57 -0500

    Lift subsystem

--- src/org/usfirst/frc/team2875/robot/subsystems/Lift.java
+++ src/org/usfirst/frc/team2875/robot/subsystems/Lift.java
@@ -1,33 +1,36 @@
 package org.usfirst.frc.team2875.robot.subsystems;
 
+import org.usfirst.frc.team2875.robot.commands.LiftCmd;
+
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
+ * :D
+ *Raffi Stinks
  */
 public class Lift extends Subsystem {
-SpeedController controller1, controller2;
-	public Lift(int pin1,int pin2) {
-   controller1 = new Talon(pin1);
-   controller2 = new Talon(pin2);
-	}
+	public static final int[][] positions = new int[4][2];
+	SpeedController controller1, controller2;
+	Encoder en1,en2;
 	
-	public void setLift(double speed) {
-		//TODO fix this depending on how they set up the lifter.
-		controller1.set(speed);
+	public Lift(int c1,int c2, int e1, int e2, int e3, int e4) {
+		controller1 = new Talon(c1);
+		controller2 = new Talon(c2);
+		en1 = new Encoder(e1,e2);
+		en2 = new Encoder(e3,e4);
+		en1.reset();
+		en2.reset();
 	}
-			
 		
 	public void stop()
 	{
 		controller1.set(0);
 	}
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new LiftCmd());
     }
 }
 
