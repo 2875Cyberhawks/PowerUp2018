8a302ab3a3a034b69a6ae749cd41b588a69cb458
Unknown <lvegna12@gmail.com>
2018-01-31 15:09:01 -0500

    Use freaking github

--- src/org/usfirst/frc/team2875/robot/subsystems/Lift.java
+++ src/org/usfirst/frc/team2875/robot/subsystems/Lift.java
@@ -5,40 +5,43 @@ import org.usfirst.frc.team2875.robot.commands.LiftCmd;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
+ * :D
+ *Raffi Stinks
  */
-public class Lift extends Subsystem {
+public class Lift extends PIDSubsystem {
 	public static final int[][] positions = new int[4][2];
-	SpeedController controller1, controller2;
-	Encoder en1,en2;
+	SpeedController controller;
+	Encoder encoder;
 	
-	public Lift(int c1,int c2, int e1, int e2, int e3, int e4) {
-		controller1 = new Talon(c1);
-		controller2 = new Talon(c2);
-		en1 = new Encoder(e1,e2);
-		en2 = new Encoder(e3,e4);
-		en1.reset();
-		en2.reset();
+	public Lift(int c1, int e1, int e2) {
+		//Name and P, I, D constants
+		super("Lift", 2.0, 0.0, 0.0);
+		this.setAbsoluteTolerance(0.05);
+		this.getPIDController().setContinuous(false);
+		controller = new Talon(c1);
 	}
 		
 	public void stop()
 	{
-		controller1.set(0);
+		controller.set(0);
 	}
+	
     public void initDefaultCommand() {
         setDefaultCommand(new LiftCmd());
     }
-    //TODO find out how many motors for box intake
-    public void boxIn(double speed) {
-    	if (speed > 0)
-    		controller2.set(1);
-    	else if (speed == 0)
-    		controller2.set(0);
-    	else if (speed<0)	
-    		controller2.set(-1);
-    }
-}
 
+	@Override
+	protected double returnPIDInput() {
+		// MIGHT HAVE TO CONVERT
+		return encoder.getRate();
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		controller.pidWrite(output);
+	}
+}
