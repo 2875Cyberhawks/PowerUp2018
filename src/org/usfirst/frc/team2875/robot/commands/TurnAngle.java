24904cd116e196ee2ea51fe68388fb4fb7469335
Unknown <ryanszaatari@gmail.com>
2018-10-04 17:34:21 -0400

    Dan finna cop a chip

--- src/org/usfirst/frc/team2875/robot/commands/TurnAngle.java
+++ src/org/usfirst/frc/team2875/robot/commands/TurnAngle.java
@@ -12,11 +12,11 @@ import edu.wpi.first.wpilibj.command.Command;
 public class TurnAngle extends Command {
 	private double goal;
 	private int direc; // -1 is left, 1 is right
-	private double deltaTheta
+	private double deltaTheta;
 	private static final double ACCEPTABLE_ANGLE = 3;
 	private static final double ROBOT_RADIUS = .32; //in m
 	private static final double MAX_TURN_SPEED = 100; //in m/s
-	private static HashMap<double><double> DUMB_STUPID_PIECEWISE; //Do not touch
+	private static HashMap<Double,Double> DUMB_STUPID_PIECEWISE; //Do not touch
 
     public TurnAngle(double goalX) {
     	super("TurnAngle");
@@ -26,11 +26,11 @@ public class TurnAngle extends Command {
     	
     	requires(Robot.dTrain);
     	
-    	DUMB_STUPID_PIECEWISE.put(180,1);
-    	DUMB_STUPID_PIECEWISE.put(90,.7);
-    	DUMB_STUPID_PIECEWISE.put(45,.5);
-    	DUMB_STUPID_PIECEWISE.put(10,.2);
-    	DUMB_STUPID_PIECEWISE.put(5,.1);
+    	DUMB_STUPID_PIECEWISE.put(180.0,1.0);
+    	DUMB_STUPID_PIECEWISE.put(90.0,.7);
+    	DUMB_STUPID_PIECEWISE.put(45.0,.5);
+    	DUMB_STUPID_PIECEWISE.put(10.0,.2);
+    	DUMB_STUPID_PIECEWISE.put(5.0,.1);
     }
 
     // Called just before this Command runs the first time
